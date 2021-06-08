09bdc03de366558bde49c0567197f9d84fa218d5
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-06-08 23:54:41 +0200

    Slider work

--- script/script.js
+++ script/script.js
@@ -27,17 +27,27 @@ let nextBtn = document.getElementById('nextImg');
 
 // ARRAY SOURCE
 
-let sourceImgSlider = ["./images/mobile-image-hero-1.jpg","./images/mobile-image-hero-2.jpg","./images/mobile-image-hero-3.jpg"];
+let sourceImgSlider = [
+"./images/mobile-image-hero-1.jpg",
+"./images/mobile-image-hero-2.jpg",
+"./images/mobile-image-hero-3.jpg"
+];
 // console.log(sourceImgSlider);
 
-let sourceTitleSlider = ["We are available all across the globe","Manufactured with the best materials","Discover innovative ways to decorate"];
+let sourceTitleSlider = [
+"We are available all across the globe",
+"Manufactured with the best materials",
+"Discover innovative ways to decorate"
+];
 // console.log(sourceTitleSlider);
 
-let sourceTextSlider = ["With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+let sourceTextSlider = [
+"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
 
 "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
 
-"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."];
+"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+];
 // console.log(sourceTextSlider);
 
 
