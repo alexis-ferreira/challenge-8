b6bdc7daa99c63241d0d6c843d920c52f0450218
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-06-08 23:53:00 +0200

    Slider work

---
+++ script/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+
+// -----------> SLIDER !!!!! RESPONSIVE !!!!!
+
+// => VARIABLES
+
+// SLIDER
+
+let imgSlider = document.getElementById('imgSlider');
+// console.log(imgSlider);
+
+let titleDescription = document.getElementById('titleDescription');
+// console.log(titleDescription);
+
+let descriptionText = document.getElementById('descriptionText');
+// console.log(descriptionText);
+
+// BUTTON
+
+let previousBtn = document.getElementById('previousImg');
+// console.log(previousBtn);
+
+let nextBtn = document.getElementById('nextImg');
+// console.log(nextBtn);
+
+
+// ARRAY SOURCE
+
+let sourceImgSlider = ["./images/mobile-image-hero-1.jpg","./images/mobile-image-hero-2.jpg","./images/mobile-image-hero-3.jpg"];
+// console.log(sourceImgSlider);
+
+let sourceTitleSlider = ["We are available all across the globe","Manufactured with the best materials","Discover innovative ways to decorate"];
+// console.log(sourceTitleSlider);
+
+let sourceTextSlider = ["With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+
+"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+
+"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."];
+// console.log(sourceTextSlider);
+
+
+// SCRIPT
+
+nextBtn.addEventListener('click', function(){
+
+    if (imgSlider.getAttribute('src') == sourceImgSlider[0]){
+        // console.log("work");
+        imgSlider.setAttribute('src', sourceImgSlider[1]);
+        titleDescription.innerHTML = sourceTitleSlider[1];
+        descriptionText.innerHTML = sourceTextSlider[1];
+
+    } else if (imgSlider.getAttribute('src') == sourceImgSlider[1]){
+        imgSlider.setAttribute('src', sourceImgSlider[2]);
+        titleDescription.innerHTML = sourceTitleSlider[2];
+        descriptionText.innerHTML = sourceTextSlider[2];
+
+    } else {
+        imgSlider.setAttribute('src', sourceImgSlider[0]);        titleDescription.innerHTML = sourceTitleSlider[0];
+        descriptionText.innerHTML = sourceTextSlider[0];
+    }
+});
+
+previousBtn.addEventListener('click', function(){
+
+    if (imgSlider.getAttribute('src') == sourceImgSlider[0]){
+        // console.log("work");
+        imgSlider.setAttribute('src', sourceImgSlider[2]);
+        titleDescription.innerHTML = sourceTitleSlider[2];
+        descriptionText.innerHTML = sourceTextSlider[2];
+
+    } else if (imgSlider.getAttribute('src') == sourceImgSlider[1]){
+        imgSlider.setAttribute('src', sourceImgSlider[0]);
+        titleDescription.innerHTML = sourceTitleSlider[0];
+        descriptionText.innerHTML = sourceTextSlider[0];
+
+    } else {
+        imgSlider.setAttribute('src', sourceImgSlider[1]);        titleDescription.innerHTML = sourceTitleSlider[1];
+        descriptionText.innerHTML = sourceTextSlider[1];
+    }
+});
