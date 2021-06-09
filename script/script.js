83b4c9b7afce5c6ea655558a801aec0c4cb8af3b
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-06-09 21:40:57 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -35,18 +35,19 @@ let sourceImgSlider = [
 // console.log(sourceImgSlider);
 
 let sourceTitleSlider = [
+"Discover innovative ways to decorate",
 "We are available all across the globe",
-"Manufactured with the best materials",
-"Discover innovative ways to decorate"
+"Manufactured with the best materials"
 ];
 // console.log(sourceTitleSlider);
 
 let sourceTextSlider = [
+
+"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+
 "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
 
-"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-
-"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
 ];
 // console.log(sourceTextSlider);
 
@@ -141,13 +142,13 @@ previousBtn.addEventListener('click', function(){
         
         if (imgSlider.getAttribute('src') == sourceImgSlider[3]){
             // console.log("work1");
-            imgSlider.setAttribute('src', sourceImgSlider[4]);
+            imgSlider.setAttribute('src', sourceImgSlider[5]);
             titleDescription.innerHTML = sourceTitleSlider[1];
             descriptionText.innerHTML = sourceTextSlider[1];
     
-        } else if (imgSlider.getAttribute('src') == sourceImgSlider[4]){
+        } else if (imgSlider.getAttribute('src') == sourceImgSlider[5]){
             // console.log("work2");
-            imgSlider.setAttribute('src', sourceImgSlider[5]);
+            imgSlider.setAttribute('src', sourceImgSlider[4]);
             titleDescription.innerHTML = sourceTitleSlider[2];
             descriptionText.innerHTML = sourceTextSlider[2];
     
